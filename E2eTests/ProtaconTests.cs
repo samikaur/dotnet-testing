9c6cd1c217f5b0226694f6d8988f4dd771f1f570
samikaur <sami.kauranen@gmail.com>
2018-02-27 22:34:11 +0200

    Added tests

--- E2eTests/ProtaconTests.cs
+++ E2eTests/ProtaconTests.cs
@@ -20,6 +20,39 @@ namespace E2eTests
 
                 Assert.Equal("Etusivu - Protacon", driver.Title);
             }
+        }
+
+        [Fact]
+        public void TestNavigationToTietoturva()
+        {            
+            using (var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)))
+            {
+                driver.Navigate().GoToUrl(@"https://www.protacon.com/");
+
+                driver.FindElementById("menu-item-1728").Click();
+
+                driver.FindElementById("menu-item-10326").Click();
+
+                Assert.Equal("Tietoturva - Protacon", driver.Title);
+            }
+        }
+        
+        [Fact]
+        public void TestUratarinatGoesToSingleUratarina()
+        {            
+            using (var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)))
+            {
+                driver.Navigate().GoToUrl(@"https://www.protacon.com/uratarinat/");
+
+                var links = driver.FindElementByClassName("main-content").FindElements(By.TagName("a"));
+
+                var pekeLink = links.First(m => m.GetAttribute("href").Contains("pekka"));
+
+                pekeLink.Click();
+
+                Assert.Equal("#13 Pekka Savolainen - Protacon", driver.Title);
+                Assert.True(driver.FindElementByClassName("main-content").Text.Contains("Millainen on Peken normipäivä?"));
+            }
         }        
     }
 }
