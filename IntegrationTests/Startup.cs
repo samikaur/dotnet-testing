7f863618c88d6f60195449b185af35a3bff92411
Sami Kauranen <sami.kauranen@protacon.com>
2018-03-01 22:19:29 +0200

    Added integration tests

---
+++ IntegrationTests/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace IntegrationTests
+{
+    public class Startup
+    {
+        private IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration config)
+        {
+            Configuration = config;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "IntegrationTests",
+                        Version = "v1"
+                    });
+            });
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "IntegrationTests");
+                c.RoutePrefix = "doc";
+            });
+        }
+    }
+}
---
+++ IntegrationTests/Tests/ProductsTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IntegrationTests.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Xunit;
+using Newtonsoft.Json;
+
+namespace IntegrationTests.Tests
+{
+    public class ProductsTests
+    {
+        private TestServer _server;
+        private HttpClient _client;
+
+        [Fact]
+        public async Task Get_ReturnsProducts()
+        {
+            Initialize();
+            await _client.GetAsync("/products");
+        }
+
+        private void Initialize()
+        {
+            _server = new TestServer(new WebHostBuilder()
+                .UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+    }
+}
