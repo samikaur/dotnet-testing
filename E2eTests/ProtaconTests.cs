9760060fec2256087bfd5b99b43439551e49b707
Sami Kauranen <sami.kauranen@protacon.com>
2018-03-01 22:40:40 +0200

    Added e2e tests.

--- E2eTests/ProtaconTests.cs
+++ E2eTests/ProtaconTests.cs
@@ -6,10 +6,32 @@ using OpenQA.Selenium.Support.UI;
 using System.IO;
 using System.Reflection;
 using System.Linq;
+using E2eTests.Pages;
 
 namespace E2eTests
 {
     public class ProtaconTests
     {
+        [Fact]
+        public void TestMainPageTitleShouldBeCorrect()
+        {
+            using (var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)))
+            {
+                var protaconPage = new ProtaconPage(driver);
+                protaconPage.Open();
+            }
+        }
+
+        [Fact]
+        public void TestNavigationToTietoturva()
+        {
+
+        }
+
+        [Fact]
+        public void TestUratarinatGoesToSingleUratarina()
+        {
+
+        }
     }
 }
